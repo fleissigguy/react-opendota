02313a83224a8418fbf20f7a3d4c2f6c23bda76a
fleissigguy <danielolofssoner@gmail.com>
2018-02-22 11:27:42 +0300

    changes publish bash script

--- github.publish.sh
+++ github.publish.sh
@@ -2,14 +2,15 @@
 rm -rf dist
 mkdir dist
 
-# clone gh-pages branch from the local repo into a repo located within "dist"
-git clone .git --branch gh-pages dist
-
 # generate contents...
 npm run build
 
-# commit the changes in the clone and push them back to the local gh-pages branch
-cd dist && git add --all && git commit -m "Publishing to gh-pages" && git push origin gh-pages
-
-# publish
-git push upstream gh-pages
+# going to our gh-pages folder
+cd dist
+git init
+git add .
+git commit -m "publish to gh-pages branch"
+git checkout -b gh-pages
+git push --force --set-upstream https://github.com/js2me/opendota-webclient.git gh-pages
+cd ..
+rm -rf dist
