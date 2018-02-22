d837724735d51772243b42d4c234bed4c5d2fdd3
fleissigguy <danielolofssoner@gmail.com>
2018-02-22 10:59:09 +0300

    publish config

--- github.publish.sh
+++ github.publish.sh
@@ -1,4 +1,15 @@
-git worktree add dist production
-(cd dist; git add .)
-(git commit -m "Build output as of $(git log '--format=format:%H' master -1)")
-git push origin production
+# remove previous publication
+rm -rf dist
+mkdir dist
+
+# clone gh-pages branch from the local repo into a repo located within "dist"
+git clone .git --branch gh-pages dist
+
+# generate contents...
+npm run build
+
+# commit the changes in the clone and push them back to the local gh-pages branch
+cd dist && git add --all && git commit -m "Publishing to gh-pages" && git push origin gh-pages
+
+# publish
+git push upstream gh-pages
