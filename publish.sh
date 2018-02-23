83bbb0731952fb478827c30d2d38c039202aa144
fleissigguy <danielolofssoner@gmail.com>
2018-02-24 01:26:19 +0300

    configuring publish.sh

--- publish.sh
+++ publish.sh
@@ -11,6 +11,6 @@ git init
 git add .
 git commit -m "publish to gh-pages branch"
 git checkout -b gh-pages
-git push --force --set-upstream https://github.com/js2me/opendota-webclient.git gh-pages
+git push --force --set-upstream https://$GH_TOKEN@github.com/js2me/opendota-webclient.git gh-pages
 cd ..
 rm -rf dist
